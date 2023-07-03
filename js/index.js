d83706891f3f0894a46e7e44c78ecc5e8d6c4045
AndresRuiu <andresruiu@gmail.com>
2023-07-02 21:56:16 -0300

    creacion de cards y semi responsive

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 fetch('../catalogo.json')
 .then(response => response.json())
     .then(data => {
+      
       const tipo = data[7].tipo;
       const hd = data[7].hd;
       const genero = data[7].genero.join(', ');
@@ -17,5 +18,40 @@ fetch('../catalogo.json')
 
       const anioSpan = document.querySelector('.anio p');
       anioSpan.textContent = anio;
-    })
-    .catch(error => console.error(error));
+
+  })
+  .catch(error => console.error(error));
+
+  async function cargarPeliculas() {
+    const response = await fetch('catalogo.json');
+    const peliculas = await response.json();
+    
+    const peliculasEstreno = peliculas.filter(pelicula => pelicula.anio.includes("proximamente"));
+    
+    const template = document.querySelector("#pelicula-template");
+    const container = document.querySelector("#peliculas-container");
+  
+    const fragment = document.createDocumentFragment();
+  
+    peliculasEstreno.forEach(pelicula => {
+      const instance = template.content.cloneNode(true);
+      
+      instance.querySelector(".poster").src = pelicula.poster;
+      instance.querySelector(".nombre").textContent = pelicula.nombre;
+      instance.querySelector(".anio").textContent = pelicula.anio[0];
+      instance.querySelector(".duracion").textContent = pelicula.duracion;
+      instance.querySelector(".ranking").textContent = pelicula.ranking;
+      instance.querySelector(".hd").textContent = "HD"
+      
+      fragment.appendChild(instance);
+    });
+  
+    container.appendChild(fragment);
+  }
+  
+  cargarPeliculas();
+  
+  
+  
+
+  
