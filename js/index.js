98a7d4753f91cff06977050c2acb17dec0fd8a36
AndresRuiu <andresruiu@gmail.com>
2023-07-05 22:46:22 -0300

    modales ya creados y funcionando

--- js/index.js
+++ js/index.js
@@ -18,7 +18,6 @@ aside.addEventListener('click', function () {
     }, 2000);
 });
 
-
 fetch('../catalogo.json')
 .then(response => response.json())
     .then(data => {
@@ -43,8 +42,62 @@ fetch('../catalogo.json')
   })
   .catch(error => console.error(error));
 
+  async function loadCatalog() {
+    const response = await fetch('../catalogo.json');
+    return await response.json();
+  }
+  
+  function createYouTubeModal(youTubeUrl, movieId) {
+    const modalContainer = document.createElement('div');
+    modalContainer.style.display = 'flex';
+    modalContainer.style.justifyContent = 'center';
+    modalContainer.style.alignItems = 'center';
+    modalContainer.style.position = 'fixed';
+    modalContainer.style.top = '0';
+    modalContainer.style.left = '0';
+    modalContainer.style.width = '100%';
+    modalContainer.style.height = '100%';
+    modalContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    modalContainer.style.backdropFilter = 'blur(10px)';
+    modalContainer.style.zIndex = '9999';
+
+    const videoContainer = document.createElement('div');
+    videoContainer.id = 'modal';
+    
+    modalContainer.addEventListener('click', event => {
+      if (event.target === modalContainer) {
+        document.body.removeChild(modalContainer);
+        if (pageContainer) {
+          pageContainer.style.filter = '';
+        }
+      }
+    });
+  
+    const iframe = document.createElement('iframe');
+    iframe.width = '560';
+    iframe.height = '315';
+    iframe.src = youTubeUrl;
+    iframe.frameBorder = '0';
+    iframe.allowFullscreen = true;
+    
+    const movieIdElement = document.createElement('div');
+    
+    videoContainer.appendChild(iframe);
+    videoContainer.appendChild(movieIdElement);
+    
+    modalContainer.appendChild(videoContainer);
+    
+    document.body.appendChild(modalContainer);
+  }
+  
+
+  
+  
+
+
+
   async function cargarPeliculasProximamente() {
-    const response = await fetch('catalogo.json');
+    const response = await fetch('../catalogo.json');
     const peliculas = await response.json();
     
     const peliculasProximamente = peliculas.filter(pelicula => pelicula.anio.includes("proximamente"));
@@ -63,6 +116,11 @@ fetch('../catalogo.json')
       instance.querySelector(".duracion").textContent = pelicula.duracion + " min";
       instance.querySelector(".ranking").textContent = pelicula.ranking;
       
+      const verMasButton = instance.querySelector('.ver-mas');
+      verMasButton.addEventListener('click', () => {
+        createYouTubeModal(pelicula.pagina, pelicula.id);
+      });
+      
       fragment.appendChild(instance);
     });
   
@@ -70,33 +128,37 @@ fetch('../catalogo.json')
   }
 
   async function cargarPeliculas2023() {
-    const response = await fetch('catalogo.json');
+    const response = await fetch('../catalogo.json');
     const peliculas = await response.json();
-    
     const peliculas2023 = peliculas.filter(pelicula => pelicula.anio == "2023" && !pelicula.anio.includes("proximamente"));
-    
     const template = document.querySelector("#pelicula-card-template");
     const container = document.querySelector("#peliculas-container-2023");
-  
     const fragment = document.createDocumentFragment();
   
     peliculas2023.forEach(pelicula => {
       const instance = template.content.cloneNode(true);
-      
       instance.querySelector(".poster").src = pelicula.poster;
       instance.querySelector(".nombre").textContent = pelicula.nombre;
       instance.querySelector(".anio").textContent = pelicula.anio;
       instance.querySelector(".duracion").textContent = pelicula.duracion + " min";
       instance.querySelector(".ranking").textContent = pelicula.ranking;
-      
+  
+      // Agregar controlador de eventos al botón ver-mas
+      const verMasButton = instance.querySelector('.ver-mas');
+      verMasButton.addEventListener('click', () => {
+        createYouTubeModal(pelicula.pagina, pelicula.id);
+      });
+  
       fragment.appendChild(instance);
     });
   
     container.appendChild(fragment);
   }
-
+  
+  
+  
   async function cargarPeliculasAD() {
-    const response = await fetch('catalogo.json');
+    const response = await fetch('../catalogo.json');
     const peliculas = await response.json();
     
     const peliculasAD = peliculas.filter(pelicula => pelicula.genero.includes("Accion") || pelicula.genero.includes("Drama"));
@@ -114,6 +176,11 @@ fetch('../catalogo.json')
       instance.querySelector(".anio").textContent = pelicula.anio;
       instance.querySelector(".duracion").textContent = pelicula.duracion + " min";
       instance.querySelector(".ranking").textContent = pelicula.ranking;
+
+      const verMasButton = instance.querySelector('.ver-mas');
+      verMasButton.addEventListener('click', () => {
+        createYouTubeModal(pelicula.pagina, pelicula.id);
+      });
       
       fragment.appendChild(instance);
     });
@@ -126,6 +193,7 @@ fetch('../catalogo.json')
   cargarPeliculasAD()
   
   
+
   const peliculasContainer2023 = document.querySelector('#peliculas-container-2023');
   const peliculasContainerAD = document.querySelector('#peliculas-container-AD');
   const scrollLeftButton = document.querySelector('#scroll-left');
